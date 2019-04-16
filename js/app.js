3df51537464b8b92c384d262a50b1ede107804e4
SerhiiFilipchuk <47062741+SerhiiFilipchuk@users.noreply.github.com>
2019-04-16 21:03:48 +0300

    carousel coments + contact btns

--- js/app.js
+++ js/app.js
@@ -1,38 +1,72 @@
-// CAROUSEL //
+// COMMENTS CAROUSEL //
 $(document).ready(function () {
-    var carousel = $("#carousel").waterwheelCarousel({
-      flankingItems: 0,
-      movingToCenter: function ($item) {
-        $('#callback-output').prepend('movingToCenter: ' + $item.attr('id') + '<br/>');
-      },
-      movedToCenter: function ($item) {
-        $('#callback-output').prepend('movedToCenter: ' + $item.attr('id') + '<br/>');
-      },
-      movingFromCenter: function ($item) {
-        $('#callback-output').prepend('movingFromCenter: ' + $item.attr('id') + '<br/>');
-      },
-      movedFromCenter: function ($item) {
-        $('#callback-output').prepend('movedFromCenter: ' + $item.attr('id') + '<br/>');
-      },
-      clickedCenter: function ($item) {
-        $('#callback-output').prepend('clickedCenter: ' + $item.attr('id') + '<br/>');
+  //rotation speed and timer
+  var speed = 5000;
+  
+  var run = setInterval(rotate, speed);
+  var slides = $('.slide');
+  var container = $('#slides ul');
+  var elm = container.find(':first-child').prop("tagName");
+  var item_width = container.width();
+  var previous = 'prev'; //id of previous button
+  var next = 'next'; //id of next button
+  slides.width(item_width); //set the slides to the correct pixel width
+  container.parent().width(item_width);
+  container.width(slides.length * item_width); //set the slides container to the correct total width
+  container.find(elm + ':first').before(container.find(elm + ':last'));
+  resetSlides();
+  
+  
+  //if user clicked on prev button
+  
+  $('#buttons a').click(function (e) {
+      //slide the item
+      
+      if (container.is(':animated')) {
+          return false;
       }
-    });
-
-    $('#prev').bind('click', function () {
-      carousel.prev();
-      return false
-    });
-
-    $('#next').bind('click', function () {
-      carousel.next();
+      if (e.target.id == previous) {
+          container.stop().animate({
+              'left': 0
+          }, 1500, function () {
+              container.find(elm + ':first').before(container.find(elm + ':last'));
+              resetSlides();
+          });
+      }
+      
+      if (e.target.id == next) {
+          container.stop().animate({
+              'left': item_width * -2
+          }, 1500, function () {
+              container.find(elm + ':last').after(container.find(elm + ':first'));
+              resetSlides();
+          });
+      }
+      
+      //cancel the link behavior            
       return false;
-    });
-
-    $('#reload').bind('click', function () {
-      newOptions = eval("(" + $('#newoptions').val() + ")");
-      carousel.reload(newOptions);
-      return false;
-    });
-
+      
   });
+  
+  //if mouse hover, pause the auto rotation, otherwise rotate it    
+  container.parent().mouseenter(function () {
+      clearInterval(run);
+  }).mouseleave(function () {
+      run = setInterval(rotate, speed);
+  });
+  
+  
+  function resetSlides() {
+      //and adjust the container so current is in the frame
+      container.css({
+          'left': -1 * item_width
+      });
+  }
+  
+});
+//a simple function to click next link
+//a timer will call this function, and the rotation will begin
+
+function rotate() {
+  $('#next').click();
+}
